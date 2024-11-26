2ba75966b1d7f93588cc64f37049febd091c970b
giga2013 <bucxrikidzegiga05@gmail.com>
2024-11-26 16:25:44 +0400

    commit

--- level 12/homework/task2.py
+++ level 12/homework/task2.py
@@ -1,8 +1,39 @@
-num1=input("enter number")
-num2=input("enter number")
-num3=float( num1 )
-print(num3)
-(num1 + num2)
-(num1 - num2)
-(num1 * num2)
-(num1 / num2)
+# ინტეჯერის მაგალითები
+
+print(9+9)
+print(9+8)
+print(9+7)
+print(9+6)
+print(9+5)
+print(9+4)
+print(9+3)
+print(9+2)
+print(9+1)
+print(9+0)
+
+# სტრინგის მაგალითები
+
+print("9+9")
+print("9+8")
+print("9+7")
+print("9+6")
+print("9+5")
+print("9+4")
+print("9+3")
+print("9+2")
+print("9+1")
+print("9+0")
+
+# ფლოატის მაგალითები
+
+print(9.9+9.9)
+print(9.9+8.8)
+print(9.9+8.8)
+print(9.9+7.7)
+print(9.9+6.6)
+print(9.9+5.5)
+print(9.9+4.4)
+print(9.9+3.3)
+print(9.9+2.2)
+print(9.9+1.1)
+print(9.9+0.0)
